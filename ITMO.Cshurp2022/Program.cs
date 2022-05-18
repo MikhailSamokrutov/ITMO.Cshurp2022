cdd1691aec08c176fa8d5c42ab602622d78ac335
MikhailSamokrutov <103500228+MikhailSamokrutov@users.noreply.github.com>
2022-05-18 14:23:20 +0300

    Add files via upload

--- ITMO.Cshurp2022/Program.cs
+++ ITMO.Cshurp2022/Program.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ITMO.Cshurp2022
+namespace ITMO.BankAccount
 {
+    
+public enum AccountType { Checking, Deposit }
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            string myName;
-            Console.WriteLine("Please enter your name");
-            myName = Console.ReadLine();
-            Console.WriteLine("Hello {0}", myName);
+            AccountType goldAccount;
 
+            AccountType platinumAccount;
 
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
 
-        
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+
         }
     }
 }
